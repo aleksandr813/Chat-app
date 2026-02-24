73cd34e9e4cd3acdd9f53e412aeca68ee56db968
aleksandr813 <molokoflina@gmail.com>
2026-02-25 01:14:15 +0400

    modified:   server/application/modules/user/User.js

--- server/application/modules/user/User.js
+++ server/application/modules/user/User.js
@@ -1,6 +1,49 @@
 class User {
-    constructor() {
-        
+    constructor(db) {
+        this.db = db;
+
+        this.username;
+        this.id;
+        this.token;
+    }
+
+    get () {
+        return {
+            username: this.username,
+            id: this.id,
+        }
+    }
+
+    getSelf() {
+        return {
+            ...this.get(),
+            token: this.token,
+        }
+    }
+
+    init({ id, username, token }) {
+        this.username = username;
+        this.id = id;
+        this.token = token;
+    }
+
+    async login(username, password) {
+        const data = await this.db.getUserByName(username);
+        if (!data) {
+            return false;
+        }
+        if (password !== data.password) {
+            return false
+        }
+
+        const token = md5(`${Math.round(Math.random() * 1000000)}`);
+        this.db.updateToken(data.id, token);
+        this.init({ ...data, token });
+        return true;
+    }
+
+    checkToken(token) {
+        return this.token === token;
     }
 }
 
